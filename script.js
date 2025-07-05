1818f9fc835d2a37c8b7ca03d268044f70410938
WhiteMonarch18 <ing.je3rf@gmail.com>
2025-07-05 18:32:30 -0400

    trouble with track, FIX SKILL TRACK

--- script.js
+++ script.js
@@ -33,3 +33,10 @@ function typeEffect() {
 document.addEventListener("DOMContentLoaded", () => {
     typeEffect(); // Start the typing effect when the DOM is fully loaded
 });
+
+// Add smooth scrolling to all links
+document.addEventListener('DOMContentLoaded', () => {
+  const track = document.getElementById('skillsTrack');
+  const clone = track.cloneNode(true); // deep clone = true
+  document.getElementById('skillsWrapper').appendChild(clone);
+});
